8f7c1f9ac42e16993c9b7e7efdac7bfd9b74b5c8
Saurav Upadhyay <sauravup041103@gmail.com>
2024-11-01 21:46:41 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl'>Excalidraw</h1>
+      <Button className='bg-sky-500 ml-2'>Hello </Button>
+    </>
+  )
+}
+
+export default App
