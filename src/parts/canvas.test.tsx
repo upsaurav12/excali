286b8c7ef405933a45ea0267a518b6bef8ca2756
Saurav Upadhyay <sauravup041103@gmail.com>
2024-11-17 17:11:10 +0530

    add: vitest and react testing library and sample test

--- src/parts/canvas.test.tsx
+++ src/parts/canvas.test.tsx
@@ -1,60 +1,42 @@
-import { it, expect, describe } from 'vitest';
+import { it, expect, describe, beforeEach } from 'vitest';
 import { Canvas } from './canvas';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import 'vitest-canvas-mock';
 
 describe('Canvas Component', () => {
-  it('Renders Canvas without Crashing', () => {
+  beforeEach(() => {
+    // Render the Canvas component once before each test
     render(<Canvas />);
+  });
 
-    // Use getByTestId to query the canvas element
-    const canvasStage = screen.getByRole('region')
+  it('Renders Canvas without Crashing', () => {
+    const canvasStage = screen.getByRole('region');
     expect(canvasStage).toBeInTheDocument();
   });
 
-  it('should render the rectangle button' ,  () => {
-    render(<Canvas/>)
-    const rectangeButton = screen.getByRole('button' , {name: 'Rectangle'})
+  it('should render the rectangle button', () => {
+    const rectangleButton = screen.getByRole('button', { name: 'Rectangle' });
+    expect(rectangleButton).toBeInTheDocument();
+  });
 
-    expect(rectangeButton).toBeInTheDocument();
-  })
+  it('should render the circle button', () => {
+    const circleButton = screen.getByRole('button', { name: 'Circle' });
+    expect(circleButton).toBeInTheDocument();
+  });
 
-  it('should render circle button' , () => {
-    render(<Canvas/>)
+  it('should render the arrow button', () => {
+    const arrowButton = screen.getByRole('button', { name: 'Arrow' });
+    expect(arrowButton).toBeInTheDocument();
+  });
 
-    const circleButton = screen.getByRole('button' , {name: 'Circle'})
+  it('should render the line button', () => {
+    const lineButton = screen.getByRole('button', { name: 'Line' });
+    expect(lineButton).toBeInTheDocument();
+  });
 
-    expect(circleButton).toBeInTheDocument()
-  })
-
-  it('should render arrow button' , () => {
-    render(<Canvas/>)
-
-    const arrowButton = screen.getByRole('button' , {name: 'Arrow'})
-
-    expect(arrowButton).toBeInTheDocument()
-  })
-
-  
-  
-  it('should rende line button' , () => {
-    render(<Canvas/>)
-
-    const lineButton = screen.getByRole('button' , {name: 'Line'})
-
-    expect(lineButton).toBeInTheDocument()
-  })
-
-  it('should render Text button' , () => {
-    render(<Canvas/>)
-
-    const textButton = screen.getByRole('button' , {name: 'Text'})
-
-    expect(textButton).toBeInTheDocument()
-  })
-
-
-  
-
+  it('should render the text button', () => {
+    const textButton = screen.getByRole('button', { name: 'Text' });
+    expect(textButton).toBeInTheDocument();
+  });
 });
